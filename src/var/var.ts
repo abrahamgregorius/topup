9112f8913a47fc70e6ebd7b22074cb084ca5e40e
Abraham <athioii6@gmail.com>
2024-09-21 13:10:43 +0700

    feat: color variables

---
+++ src/var/var.ts
@@ -0,0 +1,36 @@
+const primaryColor = "#D0FD00"
+const fontPrimaryColor = "#F9F9F9"
+const fontSecondaryColor = "#D4D4D4"
+const backgroundDarkColor = "#0C0D0D"
+const backgroundCardColor = "#151617"
+const borderColor = "#202328"
+const inputBackgroundColor = "#1D1F21"
+const inputBackgroundDisableColor = "#1B1B1C"
+const inputFontColor = "#AEAFAF"
+const inputFontDisableColor = "#545556"
+const inputIconColor = "#636465"
+const inputIconDisableColor = "#2F2F31"
+const dangerColor = "#F63030"
+const warningColor = "#FDB600"
+const successColor = "#8CF901"
+const infoColor = "#019FF9"
+
+
+export {
+    primaryColor,
+    fontPrimaryColor,
+    fontSecondaryColor,
+    backgroundDarkColor,
+    backgroundCardColor,
+    borderColor,
+    inputBackgroundColor,
+    inputBackgroundDisableColor,
+    inputFontColor,
+    inputFontDisableColor,
+    inputIconColor,
+    inputIconDisableColor,
+    dangerColor,
+    warningColor,
+    successColor,
+    infoColor,
+}
