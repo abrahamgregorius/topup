b63f22b29c7b02380ae17938346a7278025d9491
Abraham Gregorius <athioii6@gmail.com>
2024-09-24 22:02:42 +0700

    feat: homepage (not done yet)

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import Navbar from '@/components/partials/Navbar'
+import Footer from '@/components/partials/Footer'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <Navbar></Navbar>
+
+
+
+        
+        <Footer></Footer>
+    </>
+    )
+}
+
+export default Home
